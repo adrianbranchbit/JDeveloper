cfe74a183c15075bef852c1d696e0ad3b6c8015c
Cesar <Jjeronimo@branchbit.com.mx>
2019-01-31 17:19:30 -0600

    Atributos agregados a GrupoVO

--- Prueba/src/com/prueba/vo/GrupoVO.java
+++ Prueba/src/com/prueba/vo/GrupoVO.java
@@ -1,8 +1,35 @@
 package com.prueba.vo;
 
 public class GrupoVO {
+    static int id;
+    static String nombre;
+    static Double Sueldo;
     
     public GrupoVO() {
         super();
     }
+    public static void setId(int id) {
+        GrupoVO.id = id;
+    }
+
+    public static int getId() {
+        return id;
+    }
+
+    public static void setNombre(String nombre) {
+        GrupoVO.nombre = nombre;
+    }
+
+    public static String getNombre() {
+        return nombre;
+    }
+
+    public static void setSueldo(Double Sueldo) {
+        GrupoVO.Sueldo = Sueldo;
+    }
+
+    public static Double getSueldo() {
+        return Sueldo;
+    }
+
 }
