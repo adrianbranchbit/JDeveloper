eac9a6c4103f34da4482d13d0c83b15bff7e870e
Adrian Cruz <acruz@branchbit.com.mx>
2019-02-01 12:33:13 -0600

    Agregue los comentarios para generar el JavaDoc de las Clases EquipoVO, GrupoVO, ProyectoVO

--- Prueba/src/com/prueba/vo/EquipoVO.java
+++ Prueba/src/com/prueba/vo/EquipoVO.java
@@ -1,43 +1,74 @@
 package com.prueba.vo;
 
 public class EquipoVO {
+    /** Esta clase define objetos que contienen atributos especificos de un Equipo
+     * @author Adrian
+     * @since 29/01/19
+     * @version 1.0
+     */
     private String nombre;
     private String edad;
     private String sexo;
-    
+
+
+    /** Inicializamos las variables en el construtor 
+     */
     public EquipoVO() {
         super();
         nombre="";
         edad="";
         sexo="";
     }
-
+    
+    /** Asignamos un valor a la propiedad Nombre
+     * @param nombre nuevo valor para asignar a nombre
+     * */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+
+     /** Obtenemos la propiedad Nombre
+      * @return Devuelve el nombre del Equipo
+      */
     public String getNombre() {
         return nombre;
     }
 
+    /** Asignamos un valor a la propiedad Edad
+     * @param edad nuevo valor para asignar a edad
+     * */
     public void setEdad(String edad) {
         this.edad = edad;
     }
-
+    
+    /** Obtenemos la propiedad Edad
+     * @return Devuelve la edad del Equipo
+     */
     public String getEdad() {
         return edad;
     }
-
+    
+    /** Asignamos un valor a la propiedad Sexo
+     * @param sexo nuevo valor para asignar a sexo
+     * */
     public void setSexo(String sexo) {
         this.sexo = sexo;
     }
-
+    
+    /** Obtenemos la propiedad Sexo
+     * @return Devuelve el sexo del Equipo
+     */
     public String getSexo() {
         return sexo;
     }
     
+    /** Sobreescribimos el metodo toString para tener un resultado mas especifico y rapido del 
+     * objeto EquipoVO
+     * @return Devuelve nombre y la edad del Equipo
+     */
     @Override
     public String toString(){
-        return nombre+" tiene"+edad;
+        return nombre+" tiene "+edad;
     }
 }
--- Prueba/src/com/prueba/vo/GrupoVO.java
+++ Prueba/src/com/prueba/vo/GrupoVO.java
@@ -1,33 +1,67 @@
 package com.prueba.vo;
 
 public class GrupoVO {
+    /**
+     * Definicion de la clase GrupoVo
+     * @author Jeronimo
+     * @since 31/01/19
+     * @version 1.0
+     */
     static int id;
     static String nombre;
     static Double Sueldo;
-    
+
+    /**
+     * Constructor de la clase
+     */
     public GrupoVO() {
         super();
     }
+
+    /**
+     *Se asigna un valor a la propiedad id
+     * @param id Tipo entero
+     */
     public static void setId(int id) {
         GrupoVO.id = id;
     }
 
+    /**
+     * Obtenemos el id
+     * @return id del Grupo
+     */
     public static int getId() {
         return id;
     }
 
+    /**
+     * Se asigna un valor a la propiedad nombre
+     * @param nombre Tipo cadena de caracteres
+     */
     public static void setNombre(String nombre) {
         GrupoVO.nombre = nombre;
     }
 
+    /**
+     * Obtenemos el nombre
+     * @return nombre del grupo
+     */
     public static String getNombre() {
         return nombre;
     }
 
+    /**
+     *Se asigna un valor a la propiedad sueldo
+     * @param Sueldo Tipo Double 
+     */
     public static void setSueldo(Double Sueldo) {
         GrupoVO.Sueldo = Sueldo;
     }
 
+    /**
+     *Obtenemos el sueldo del Grupo
+     * @return sueldo del Grupo, con decimales
+     */
     public static Double getSueldo() {
         return Sueldo;
     }
